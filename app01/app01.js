111ed7c0d361747e7845d06324b5082fea2767df
Kato Damian <katodakam2010@gmail.com>
2021-06-22 20:05:12 -0600

    Made improvements, yes

--- app01/app01.js
+++ app01/app01.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 app.set("port", 5050);
+app.get("/", function(req, res) {
+    res.status(200).send("App01 Server running on port 5050");
+
+})
 const server = app.listen(app.get("port"), function() {
 
     console.log("Listening on port", server.address().port);
