b373d2f25c0115c690c4b0704a9debb4dedf566c
Kato Damian <katodakam2010@gmail.com>
2021-06-22 20:41:10 -0600

    Improved code readability

--- app03/controllers/playController.js
+++ app03/controllers/playController.js
@@ -1,7 +1,7 @@
 module.exports.MultiplyNums = function(req, res) {
     let num1 = parseInt(req.params.x);
     let num2 = parseInt(req.query.y);
-    let results = num1+num2;
+    let results = num1*num2;
 
     console.log(`multiplying two numbers ${num1} and ${num2} yields ${results}`)
 
